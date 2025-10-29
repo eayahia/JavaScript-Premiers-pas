9f1abc3ec2a17101cd9b1520b41268ef910c21df
Yahia El Afia <yahiaelafia1@gmail.com>
2025-10-29 11:12:58 +0100

    JS

--- Bibliothèque en ligne/Chapitre2/script.js
+++ Bibliothèque en ligne/Chapitre2/script.js
@@ -5,6 +5,6 @@ let bouton = document.querySelector(".btn");
 bouton.addEventListener("click", function() {
   titre.innerText = "Titre modifié !";
   titre.classList.toggle("highlight");
-  image.setAttribute("src", "photo.png");
+  image.setAttribute("src", "assets/jeune-bel-homme-isole-dans-poses-differentes-fond-blanc-illustration_632498-859.jpg");
   image.setAttribute("alt", "Nouvelle image");
 });
