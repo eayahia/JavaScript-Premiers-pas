0ad1e71984f7bc62a055381213fb29e3c6b2634d
Yahia El Afia <yahiaelafia1@gmail.com>
2025-11-05 16:01:00 +0100

    JS

--- Bibliothèque en ligne/Prototype/main.js
+++ Bibliothèque en ligne/Prototype/main.js
@@ -1,27 +1,33 @@
-const bibliotheque = [];
+const bibliotheque = [
+     { code:12,titre: "Clean Code", auteur: "Robert C. Martin", annee: 2008,checkbox: true, prix: 150 },
+
+  {code:45, titre: "Eloquent JavaScript", auteur: "Marijn Haverbeke", annee: 2018, checkbox: true, prix: 200 },
+];
 document.getElementById("ajouter").addEventListener("click",function(){
     window.open("ajouter.html");
 });
 
 
 
-function afficher() {
+function afficher(filtres = bibliotheque) {
 
     let listes = document.getElementById("listeslivres");
     listes.innerHTML="";
 
-    bibliotheque.forEach(livre=>{
+    filtres.forEach(livre=>{
     let carte = document.createElement("div");
+    carte.classList.add("carte")
     listes.appendChild(carte);
     carte.innerHTML =
      `<h1>LE LIVRE </h1>
-<p> Le Nom de livre ${livre.titre}</p>
-<p> Le Nom de l'auteur ${livre.auteur}</p>
-<p> L'annee de sortie de livre ${livre.annee}</p>
-<p> Le prix de livre ${livre.prix}
-<p> Status : ${livre.checkbox?"disponible":"pas disponible"}`;
+<p> Le Nom de livre : ${livre.titre}</p>
+<p> Le Nom de l'auteur : ${livre.auteur}</p>
+<p> L'annee de sortie de livre : ${livre.annee}</p>
+<p> Le prix de livre : ${livre.prix}</p>
+<p> Status : ${livre.checkbox?"disponible":"pas disponible"}</p>`;
 
 let btn = document.createElement("button");
+btn.classList.add("btn")
 btn.textContent="Supprimer";
 carte.appendChild(btn);
 btn.addEventListener("click", function() {
@@ -55,30 +61,9 @@ search.addEventListener("input", function() {
     // Filtrer les livres selon le texte tapé
     let filtres = bibliotheque.filter(livre =>
         livre.titre.toLowerCase().includes(text)
+        
     );
-
-    // Afficher uniquement les livres filtrés
-    let listes = document.getElementById("listeslivres");
-    listes.innerHTML = "";
-
-    filtres.forEach(livre => {
-        let carte = document.createElement("div");
-        listes.appendChild(carte);
-        carte.innerHTML = `
-            <h1>LE LIVRE</h1>
-            <p>Le Nom de livre ${livre.titre}</p>
-            <p>Le Nom de l'auteur ${livre.auteur}</p>
-            <p>L'année de sortie de livre ${livre.annee}</p>
-            <p>Le prix de livre ${livre.prix}</p>
-            <p>Status : ${livre.checkbox ? "disponible" : "pas disponible"}</p>
-        `;
-        let btn = document.createElement("button");
-        btn.textContent = "Supprimer";
-        carte.appendChild(btn);
-        btn.addEventListener("click", function() {
-            supprimer(livre.code);
-        });
-    });
+afficher(filtres);
 });
 
 
@@ -99,7 +84,7 @@ function statistic() {
 
 function ajouter(livre) {
    bibliotheque.push(livre);
-afficher(); 
+afficher(bibliotheque); 
 }
 
 
--- Bibliothèque en ligne/Prototype/script.js
+++ Bibliothèque en ligne/Prototype/script.js
@@ -1,14 +1,14 @@
 let form = document.querySelector("form");
 form.addEventListener(("submit"),function(E){
     E.preventDefault()
-let code = document.getElementById("code").value;
-let titre = document.getElementById("titre").value;
-let auteur = document.getElementById("auteur").value;
-let annee = document.getElementById("annee").value;
-let prix = document.getElementById("prix").value;
-let checkbox = document.getElementById("disponible").checked;
+let code = document.getElementById("code-id").value;
+let titre = document.getElementById("titre-id").value;
+let auteur = document.getElementById("auteur-id").value;
+let annee = document.getElementById("annee-id").value;
+let prix = document.getElementById("prix-id").value;
+let checkbox = document.getElementById("disponible-id").checked;
 
-let livre={
+let livre ={
     code,titre,auteur,annee,prix,checkbox
 }
 
